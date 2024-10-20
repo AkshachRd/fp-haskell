76638574f96da6d454e08b3b9ec1bc19b645b1d3
Daniil <dr.chashka@gmail.com>
2024-10-20 23:18:56 +0300

    Поправил форматирование.

--- lab05/task3/editor.hs
+++ lab05/task3/editor.hs
@@ -6,28 +6,29 @@ import Data.Char(isDigit)
 
 viewFile :: String -> IO ()
 viewFile inFileName = do
-  content <- readFile inFileName
-  putStrLn $ "Файл " ++ show inFileName ++ " содержит следующую информацию:"
-  putStrLn content
+    content <- readFile inFileName
+    putStrLn $ "Файл " ++ show inFileName ++ " содержит следующую информацию:"
+    putStrLn content
 
 appendDataFile :: String -> String -> IO ()
 appendDataFile inFileName newContent = do
-  appendFile inFileName (newContent ++ "\n")
-  putStrLn $ "В файл " ++ show inFileName ++ " была добавлена строка:" ++ show newContent
+    appendFile inFileName (newContent ++ "\n")
+    putStrLn $ "В файл " ++ show inFileName ++ " была добавлена строка:" ++ show newContent
 
 removeLineFromFile :: String -> Int -> IO ()
 removeLineFromFile inFileName lineNumber = do
-  if lineNumber <= 0
+    if lineNumber <= 0
     then putStrLn "Недопустимый номер строки"
     else do
-      input <- readFile inFileName
-      let lineList = lines input
-      if length lineList < lineNumber
+        input <- readFile inFileName
+        let lineList = lines input
+        if length lineList < lineNumber
         then putStrLn "Номер строки привышает количество строк в файле"
         else do
-          let output = unlines $ delete (lineList !! (lineNumber - 1)) lineList
-          writeFile inFileName output
-          putStrLn $ "Из файла " ++ show inFileName ++ " была удалена строка с номером:" ++ show lineNumber
+            let line = lineList !! (lineNumber - 1)
+            let output = unlines $ delete line lineList
+            writeFile inFileName output
+            putStrLn $ "Из файла " ++ show inFileName ++ " была удалена строка с номером:" ++ show lineNumber
 
 copyFileWithBlankLinesFilter :: String -> String -> IO ()
 copyFileWithBlankLinesFilter inFileName outputFileName = do
@@ -46,33 +47,33 @@ copyFileWithKeyWordFilter inFileName outputFileName keyword  = do
 
 main :: IO ()
 main = do
-  args <- getArgs
-  if length args < 1
+    args <- getArgs
+    if length args < 1
     then putStrLn "Specify the file name"
     else do
-    let [inputFileName] = args
-    inputFileExists <- doesFileExist inputFileName
-    if not inputFileExists
-      then putStrLn $ "File " ++ inputFileName ++ " does not exist"
-      else do
-        putStrLn "Выберите операцию: 1 - Просмотр, 2 - Добавление, 3 - Удаление, 4 - Копирование с фльтрацией пустых строк"
-        operation <- getLine
-        case (operation) of
-          "1" -> viewFile inputFileName
-          "2" -> do
-            putStrLn "Введите строку, которую хотите добавить: "
-            newContent <- getLine
-            appendDataFile inputFileName newContent
-          "3" -> do
-            putStrLn "Введите номер строки, которую хотите удалить: "
-            lineNumber <- getLine
-            removeLineFromFile (inputFileName) (read (lineNumber) :: Int)
-          "4" -> do
-            let outputFileName = if length args < 2 then inputFileName ++ " (copy)" else args !! 1
-            copyFileWithBlankLinesFilter inputFileName outputFileName
-          "5" -> do
-            putStrLn "Введите строку, которую хотите добавить: "
-            keyword <- getLine
-            let outputFileName = if length args < 2 then inputFileName ++ " (copy)" else args !! 1
-            copyFileWithKeyWordFilter inputFileName outputFileName keyword
-          _ -> putStrLn "Неверная операция"
+        let [inputFileName] = args
+        inputFileExists <- doesFileExist inputFileName
+        if not inputFileExists
+        then putStrLn $ "File " ++ inputFileName ++ " does not exist"
+        else do
+            putStrLn "Выберите операцию: 1 - Просмотр, 2 - Добавление, 3 - Удаление, 4 - Копирование с фльтрацией пустых строк"
+            operation <- getLine
+            case (operation) of
+                "1" -> viewFile inputFileName
+                "2" -> do
+                    putStrLn "Введите строку, которую хотите добавить: "
+                    newContent <- getLine
+                    appendDataFile inputFileName newContent
+                "3" -> do
+                    putStrLn "Введите номер строки, которую хотите удалить: "
+                    lineNumber <- getLine
+                    removeLineFromFile (inputFileName) (read (lineNumber) :: Int)
+                "4" -> do
+                    let outputFileName = if length args < 2 then inputFileName ++ " (copy)" else args !! 1
+                    copyFileWithBlankLinesFilter inputFileName outputFileName
+                "5" -> do
+                    putStrLn "Введите строку, которую хотите добавить: "
+                    keyword <- getLine
+                    let outputFileName = if length args < 2 then inputFileName ++ " (copy)" else args !! 1
+                    copyFileWithKeyWordFilter inputFileName outputFileName keyword
+                _ -> putStrLn "Неверная операция"
